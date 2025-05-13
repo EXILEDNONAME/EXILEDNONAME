e2b90fe83463f207d83001727cbf1ee60b9368c3
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-05-14 00:38:16 +0700

    Update #1

---
+++ database/seeders/Access/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders\Access;
+
+use App\Models\User;
+use Carbon\Carbon as Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder {
+
+  public function run(): void {
+    $user = [
+      [
+        'name'        => 'Administrator',
+        'email'       => 'admin@exilednoname.com',
+        // 'phone'       => '0811',
+        // 'username'    => 'administrator',
+        'password'    => bcrypt('1234'),
+        'created_at'  => Carbon::now(),
+      ],
+      ];
+    User::insert($user);
+  }
+}
