13a333ec8ef8f468d47d9be013c15240d4c8a6c0
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-05-14 00:47:07 +0700

    Update #2

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +16,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/dashboard', function () {
+    return view('pages.backend.dashboard');
+});
