de35e3dbbfebb5c42113778290abf49b781ea70a
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-05-23 18:37:38 +0700

    Update #37

--- app/Models/Backend/__System/Application/Datatable/Relation.php
+++ app/Models/Backend/__System/Application/Datatable/Relation.php
@@ -5,8 +5,8 @@ namespace App\Models\Backend\__System\Application\Datatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Relation extends Model {
 
