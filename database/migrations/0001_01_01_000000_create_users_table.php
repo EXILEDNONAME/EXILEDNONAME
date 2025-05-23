685f283997bbe7759763a7c3e18f75d66aa31ad1
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-05-23 19:54:03 +0700

    Update #41

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('avatar')->nullable();
             $table->string('name');
             $table->string('email')->unique();
