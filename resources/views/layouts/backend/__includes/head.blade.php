122862328184314007385260a4e058bf29264dbf
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-09-09 11:30:40 +0700

    2

---
+++ resources/views/layouts/backend/__includes/head.blade.php
@@ -0,0 +1,17 @@
+<head>
+  <base href="../../">
+  <meta charset="utf-8" />
+  <title> EXILEDNONAME </title>
+  <meta name="description" content="Page with empty content" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+  <link href="{{ env('APP_URL') }}/assets/backend/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="{{ env('APP_URL') }}/assets/backend/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="{{ env('APP_URL') }}/assets/backend/css/style.bundle.css" rel="stylesheet" type="text/css" />
+  <link href="{{ env('APP_URL') }}/assets/backend/css/themes/layout/header/base/light.css" rel="stylesheet" type="text/css" />
+  <link href="{{ env('APP_URL') }}/assets/backend/css/themes/layout/header/menu/light.css" rel="stylesheet" type="text/css" />
+  <link href="{{ env('APP_URL') }}/assets/backend/css/themes/layout/brand/dark.css" rel="stylesheet" type="text/css" />
+  <link href="{{ env('APP_URL') }}/assets/backend/css/themes/layout/aside/dark.css" rel="stylesheet" type="text/css" />
+  <link rel="shortcut icon" href="{{ env('APP_URL') }}/assets/backend/media/logos/favicon.ico" />
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
